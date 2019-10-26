d75ed8d41350a3ec6b1c4bd2d30b3d55534a7674
konectiait <56847744+konectiait@users.noreply.github.com>
2019-10-26 11:34:34 -0300

    Add files via upload

---
+++ App_Start/WebApiConfig.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web.Http;
+using Newtonsoft.Json.Serialization;
+using System.Web.Http.Cors;
+using Newtonsoft.Json;
+
+namespace MundoCanjeWeb
+{
+    public static class WebApiConfig
+    {
+        //public static void Register(HttpConfiguration config)
+        //{
+        //    // Configuración y servicios de API web
+
+        //    // Rutas de API web
+        //    config.MapHttpAttributeRoutes();
+
+        //    config.Routes.MapHttpRoute(
+        //        name: "DefaultApi",
+        //        routeTemplate: "api/{controller}/{id}",
+        //        defaults: new { id = RouteParameter.Optional }
+        //    );
+        //}
+        public static void Register(HttpConfiguration config)
+        {
+            //config.SuppressDefaultHostAuthentication();
+            //config.Filters.Add(new HostAuthenticationFilter(OAuthDefaults.AuthenticationType));
+            var cors = new EnableCorsAttribute("*", "*", "*");
+            config.EnableCors(cors);
+            // Rutas de Web API
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+            config.Routes.MapHttpRoute(
+                name: "ApiByName",
+                routeTemplate: "api/{controller}/{action}/{name}",
+                defaults: null,
+                constraints: new { name = @"^[a-z]+$" }
+            );
+
+            config.Routes.MapHttpRoute(
+                name: "ApiByAction",
+                routeTemplate: "api/{controller}/{action}",
+                defaults: new { action = "Get" }
+            );
+            config.Routes.MapHttpRoute(
+                      "ApiPluAndidSuc",
+                      "api/{controller}/{action}/{Plu}/{idSucursal}"
+            );
+
+
+            var json = config.Formatters.JsonFormatter;
+            config.Formatters.JsonFormatter.SerializerSettings = new JsonSerializerSettings();
+            json.SerializerSettings.PreserveReferencesHandling = Newtonsoft.Json.PreserveReferencesHandling.Objects;
+            config.Formatters.Remove(config.Formatters.XmlFormatter);
+        }
+    }
+}
