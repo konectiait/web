e9bc8f0fca0d3a5fe14d57b03070f38fccc304c4
Facundo <facundo.caden@gmail.com>
2019-11-07 10:58:44 -0300

    class loca

---
+++ Models/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MundoCanjeWeb.Models
+{
+    public class Class1
+    {
+        public int prueba { get; set; }
+    }
+}
